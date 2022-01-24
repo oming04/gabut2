69e1018e68c5f3a7966f5914568df6c5b9556179
oming04 <96978897+oming04@users.noreply.github.com>
2022-01-24 19:41:00 +0800

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,6 +92,7 @@ async def start_command(client: Client, message: Message):
                 [
                     InlineKeyboardButton("😊 Kepo Ya?", callback_data = "about"),
                     InlineKeyboardButton("🔒 Tutup", callback_data = "close")
+                    InlineKeyboardButton("JOIN VIP", callback_data = "vip")
                 ]
             ]
         )
@@ -108,27 +109,7 @@ async def start_command(client: Client, message: Message):
             quote = True
         )
         return
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("JOIN VIP", callback_data = "vip")
-                ]
-            ]
-        )
-        await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
-        return
-
+        
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     buttons = [
